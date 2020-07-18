f59ce2a46943fbe7488aa7cf4d74dde7025ad147
Diogo Souza <diogo.souza@msn.com>
2020-07-18 19:53:48 -0400

    API Recomendações
    removido o baseURL

--- api-recomendacoes/src/recommendation/recommendation.module.ts
+++ api-recomendacoes/src/recommendation/recommendation.module.ts
@@ -1,13 +1,13 @@
 import { HttpModule, Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { RecommendationController } from './recommendation.controller';
 import { RecommendationService } from './recommendation.service';
-import { parseToInt } from "../utils";
 import { MicroserviceModule } from "../microservice/microservice.module";
 
 @Module({
   imports: [
+    ConfigModule,
     HttpModule.register({
-      baseURL: `${process.env.CATALOGO_HOST}:${parseToInt(process.env.CATALOGO_PORT)}`,
       timeout: 5000,
     }),
     MicroserviceModule,
