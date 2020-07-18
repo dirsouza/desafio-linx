7f3bd0126326fdafbe8b4a8a05191a69fda982ae
Diogo Souza <diogo.souza@msn.com>
2020-07-18 10:15:43 -0400

    API Recomendaçẽos
    adicionado configurações da API

--- api-recomendacoes/src/main.ts
+++ api-recomendacoes/src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { parseToInt } from './utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('recomendacao');
+  app.use(compression());
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+  });
+
+  const configService = app.get(ConfigService);
+
+  SwaggerModule.setup(
+    'api-recomendacao',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('API Recomendação')
+        .setDescription('API para fornecer dados de Recomendações')
+        .setVersion('1.0')
+        .build(),
+    ),
+  );
+
+  await app.listen(
+    parseToInt(configService.get<string>('RECOMENDACAO_PORT')) || 3001,
+  );
+  const getUrl = await app.getUrl();
+
+  Logger.verbose(`API Recomendação is running on: ${getUrl}`);
+  Logger.verbose(`Document API Recomendação is running on: ${getUrl}/api-recomendacao`);
 }
 bootstrap();
