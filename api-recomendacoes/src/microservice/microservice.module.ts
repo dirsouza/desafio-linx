4400c88b465cec360d4d093b371b2e2535138861
Diogo Souza <diogo.souza@msn.com>
2020-07-18 19:53:17 -0400

    API Recomendações
    removido baseURL do módulo e injetado o baseURL na criação do service

--- api-recomendacoes/src/microservice/microservice.module.ts
+++ api-recomendacoes/src/microservice/microservice.module.ts
@@ -1,10 +1,11 @@
 import { HttpModule, Module } from "@nestjs/common";
 import { MicroserviceService } from './microservice.service';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule,
     HttpModule.register({
-      baseURL: process.env.MICROSERVICE_HOST,
       timeout: 5000,
     }),
   ],
--- api-recomendacoes/src/microservice/microservice.service.ts
+++ api-recomendacoes/src/microservice/microservice.service.ts
@@ -1,10 +1,32 @@
 import { HttpService, Injectable } from "@nestjs/common";
+import { AxiosResponse } from "axios";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class MicroserviceService {
-  constructor(private readonly httpService: HttpService) {}
+  private readonly baseUrl: string;
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService
+  ) {
+    this.baseUrl = this.configService.get<string>('MICROSERVICE_HOST');
+  }
 
-  findRecommendations(ranking: string) {
-    return this.httpService.get(`${ranking}.json`);
+  async findRecommendations(ranking: string): Promise<AxiosResponse<number[]> | void> {
+    try {
+      return await this.httpService.get(`${this.baseUrl}/${ranking}.json`)
+        .toPromise()
+        .then(({ data }) =>
+          Object.assign( [],
+            data.map(prod => prod.recommendedProduct)
+              .map(prod => prod.id)
+          )
+        )
+        .catch(err => {
+          throw err;
+        });
+    } catch (e) {
+      throw e;
+    }
   }
 }
