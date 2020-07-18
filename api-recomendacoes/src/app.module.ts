b8dac9b2691e76f7d65f1787a17bb7f97fccd43d
Diogo Souza <diogo.souza@msn.com>
2020-07-18 10:29:21 -0400

    API Recomendações
    remoção de configurações de conexão com database

--- api-recomendacoes/src/app.module.ts
+++ api-recomendacoes/src/app.module.ts
@@ -1,23 +1,11 @@
 import { Module, CacheModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import * as redisStore from 'cache-manager-redis-store';
 import { parseToInt, parseTypeDB } from './utils';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: parseTypeDB(process.env.DB_TYPE),
-      host: process.env.DB_HOST,
-      port: parseToInt(process.env.DB_PORT) || 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [],
-      logging: ['error'],
-      autoLoadEntities: true,
-    }),
     CacheModule.register({
       store: redisStore,
       host: process.env.CACHE_HOST,
